deebdc7e4e2a64a7c4f602b27247665bd3a0c843
lcj446068124 <31842914+lcj446068124@users.noreply.github.com>
2021-02-21 22:28:05 +0800

    init
    init

---
+++ jflash.bat
@@ -0,0 +1,12 @@
+@echo off
+set n=1
+:loop
+"C:\Program Files (x86)\SEGGER\JLink_V490\JFlash.exe" -open"C:\Program Files (x86)\SEGGER\JLink_V490\Samples\JFlash\ProjectFiles\ST\STM32F103RC.jflash" -connect -open"C:\Users\44606\Desktop\Pump01_FW\MDK-ARM\Pump01\Pump01.hex" -erasechip -programverify -startapplication -jflashloglog.log -exit
+if %errorlevel% equ 0 (
+set /a n=%n%+1
+set /p a=succeed:%n%
+) else (
+find "ERROR" jlink.log
+ set /p a = failed!!
+)
+if "%a%"=="" goto loop
